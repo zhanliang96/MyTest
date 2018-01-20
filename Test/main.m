7e34b80f9322f1772c7f3c007e6345e598a51d8d
zhanliang1996 <zhanliang1996@gmail.com>
2018-01-20 19:41:53 +0800

    Initial Commit

---
+++ Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by 展亮 on 2018/1/20.
+//  Copyright © 2018年 展亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
